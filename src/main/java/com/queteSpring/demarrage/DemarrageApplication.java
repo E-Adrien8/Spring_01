f2e66e859fe3332fa7c1b3b427e06b589e05c328
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-10-31 12:05:44 +0100

    final

---
+++ src/main/java/com/queteSpring/demarrage/DemarrageApplication.java
@@ -0,0 +1,13 @@
+package com.queteSpring.demarrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemarrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemarrageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queteSpring/demarrage/DemarrageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queteSpring.demarrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemarrageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
